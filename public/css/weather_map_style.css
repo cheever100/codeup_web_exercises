809ab56d2bfdf2d2f4240cf32999328f42a333bd
Jean Cheever <jeancheever@Jeans-MacBook-Air.local>
2016-02-18 16:00:12 -0600

    various exercises Feb 17 and 18

---
+++ public/css/weather_map_style.css
@@ -0,0 +1,38 @@
+
+	.labelCenter{
+		text-align: center;
+	}
+	
+	table {
+	    border-collapse: collapse;
+	    width: 100%;
+	}
+	
+	table, th, td {
+    	border: 1px solid black;
+	}
+	
+	th {
+	    height: 50px;
+	    background-color: yellow;
+	}
+
+	td {
+		height: 50px;
+	}
+
+	tr:nth-child(even){background-color: yellow}
+
+	button {
+		height: 50px;
+		width: 100px;
+		background-color: #00FF00;
+		font-style: italic;
+	}
+
+    #map-canvas {
+        width: 800px;
+        height: 400px;
+    }    
+
+    </style>
